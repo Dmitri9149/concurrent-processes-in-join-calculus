ec690b9b9b6f1f6b263dba1e9a4c505d15dccc3d
dmitri9149 <dmitri.as.name@gmail.com>
2020-08-07 11:03:10 +0300

    locks struckture

---
+++ locks/locks.ml
@@ -0,0 +1,29 @@
+(* Lock structure *)
+
+let new_lock () = 
+        def free() & lock() = reply to lock
+        and unlock() = free() & reply to unlock in 
+        spawn free() ;
+        lock, unlock
+;;
+
+
+(* this function outputs its string argument several time *)
+
+let print_n n s = 
+        for i = 1 to n do 
+                print_string s; Thread.delay 0.01
+        done
+;;
+
+spawn (print_n 21 "*" ; 0) & (print_n 21 "+" ; 0)
+;;
+
+let lock, unlock = new_lock()
+;;
+spawn begin
+        (lock() ; print_n 21 "*" ; unlock() ; 0)
+      & (lock() ; print_n 21 "+" ; unlock() ; 0)
+end
+;;
+
